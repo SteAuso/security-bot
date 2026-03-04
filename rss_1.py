27b6efe848b25015290b2838a76d4e648315de2e
SteAuso <61969709+SteAuso@users.noreply.github.com>
2026-03-04 16:00:02 +0100

    Update rss_1.py

--- rss_1.py
+++ rss_1.py
@@ -5,14 +5,13 @@ import os
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
 RSS_URL = "https://www.ransomware.live/rss.xml"
 FILE_HISTORY = "history.txt"
-LIMIT = 50
+LIMIT = 200
 
 def main():
     feed = feedparser.parse(RSS_URL)
     if not feed.entries:
         return
 
-    # Carica la cronologia esistente
     if os.path.exists(FILE_HISTORY):
         with open(FILE_HISTORY, "r") as f:
             history = [line.strip() for line in f.readlines()]
@@ -21,8 +20,6 @@ def main():
 
     nuovi_inviati = False
     
-    # 2. Esamina le news (dalla più vecchia alla più recente per l'ordine cronologico su Discord)
-    # feed.entries è solitamente ordinato dal più nuovo al più vecchio, quindi lo invertiamo
     for entry in reversed(feed.entries):
         link = entry.link
         
@@ -37,9 +34,8 @@ def main():
             history.append(link)
             nuovi_inviati = True
 
-    # 3. Mantieni solo gli ultimi 50 link e salva
     if nuovi_inviati:
-        history = history[-LIMIT:] # Prende gli ultimi 50
+        history = history[-LIMIT:]
         with open(FILE_HISTORY, "w") as f:
             for item in history:
                 f.write(f"{item}\n")
