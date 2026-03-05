8ca03dcc6477584c90f605cbe101e013210ee46a
SteAuso <61969709+SteAuso@users.noreply.github.com>
2026-03-05 18:44:36 +0100

    Update rss_2.py

--- rss_2.py
+++ rss_2.py
@@ -5,7 +5,7 @@ import os
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_2")
 RSS_URL = "https://www.acn.gov.it/portale/feedrss/-/journal/rss/20119/723192"
 FILE_HISTORY = "history_2.txt"
-LIMIT = 10
+LIMIT = 100
 
 def main():
     feed = feedparser.parse(RSS_URL)
