39bbbc24374caef9b62abba9c2ff0540d6b9d26c
SteAuso <61969709+SteAuso@users.noreply.github.com>
2026-03-05 16:27:12 +0100

    Update rss_2.py

--- rss_2.py
+++ rss_2.py
@@ -26,7 +26,7 @@ def main():
         if link not in history:
             print(f"Nuova news: {entry.title}")
             
-            message = f"**{entry.title}**\n{entry.description}\n\nCountry: {entry.category}\n\n{link}"
+            message = f"**{entry.title}**\n{entry.description}\n\Publication date: {entry.pubDate}\n\n{link}"
             requests.post(WEBHOOK_URL, json={"content": message})
             
             history.append(link)
