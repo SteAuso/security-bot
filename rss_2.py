860dac42fcc060cb8f2254f557e921aaf5e3a375
SteAuso <61969709+SteAuso@users.noreply.github.com>
2026-03-05 16:00:58 +0100

    Create rss_2.py

---
+++ rss_2.py
@@ -0,0 +1,44 @@
+import feedparser
+import requests
+import os
+
+WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
+RSS_URL = "https://www.ransomware.live/rss.xml"
+FILE_HISTORY = "history.txt"
+LIMIT = 200
+
+def main():
+    feed = feedparser.parse(RSS_URL)
+    if not feed.entries:
+        return
+
+    if os.path.exists(FILE_HISTORY):
+        with open(FILE_HISTORY, "r") as f:
+            history = [line.strip() for line in f.readlines()]
+    else:
+        history = []
+
+    nuovi_inviati = False
+    
+    for entry in reversed(feed.entries):
+        link = entry.link
+        
+        if link not in history:
+            print(f"Nuova news: {entry.title}")
+            
+            message = f"**{entry.title}**\n{entry.description}\n\nCountry: {entry.category}\n\n{link}"
+            requests.post(WEBHOOK_URL, json={"content": message})
+            
+            history.append(link)
+            nuovi_inviati = True
+
+    if nuovi_inviati:
+        history = history[-LIMIT:]
+        with open(FILE_HISTORY, "w") as f:
+            for item in history:
+                f.write(f"{item}\n")
+    else:
+        print("Nulla di nuovo da inviare.")
+
+if __name__ == "__main__":
+    main()
